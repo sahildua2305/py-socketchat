b41d1774f80a05a437bf281230aafd3f748fc99b
sahildua2305 <sahildua2305@gmail.com>
2014-04-25 23:06:40 +0530

    final changes

---
+++ chat_client.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import socket, select, string, sys
+
+def prompt():
+	sys.stdout.write('<You> ')
+	sys.stdout.flush()
+
+if __name__ == "__main__":
+	if(len(sys.argv) < 3):
+		print 'Please use correct syntax : python chat_client.py hostname port'
+		sys.exit()
+	host = sys.argv[1]
+	port = int(sys.argv[2])
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(2)
+	
+	#connect to remote host
+	try:
+		s.connect((host, port))
+	except:
+		print 'Unable to connect'
+		sys.exit()
+	
+	print 'Connected to remote host. Start sending messages!'
+	prompt()
+	
+	while 1:
+		socket_list = [sys.stdin, s]
+		
+		#get the list sockets which are readable
+		read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+		
+		for sock in read_sockets:
+			#incoming message from remote server
+			if sock == s:
+				data = sock.recv(4096)
+				if not data:
+					print '\nDisconnected from the chat server'
+					sys.exit()
+				else:
+					#print data
+					sys.stdout.write(data)
+			
+			#user entered a message
+			else:
+				msg = sys.stdin.readline()
+				s.send(msg)
+				prompt()
+
+			
