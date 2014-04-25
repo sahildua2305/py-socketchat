6a375f89bf9e5f3d75fd28a010663966384be21e
sahildua2305 <sahildua2305@gmail.com>
2014-04-25 23:05:55 +0530

    added forgotten bind()

--- chat_server.py
+++ chat_server.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
 	
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server_socket.bind(("0.0.0.0", PORT))
 	server_socket.listen(10)
 	
 	CONNECTION_LIST.append(server_socket)
